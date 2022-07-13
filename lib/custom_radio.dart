da14bae6919ca533edd822495464c9130421f5b5
yanyangyang <yanyangyang@uenpay.com>
2022-07-13 21:48:57 +0800

    初始代码

---
+++ lib/custom_radio.dart
@@ -0,0 +1,11 @@
+library custom_radio;
+import 'package:flutter/material.dart';
+
+class CustomRadio extends StatelessWidget  {
+  const CustomRadio({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('测试组件');
+  }
+}
