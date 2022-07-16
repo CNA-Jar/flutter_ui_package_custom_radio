e34142d00bd611a5f2fe46ee9914423488557776
yanyangyang <yanyangyang@uenpay.com>
2022-07-16 19:53:24 +0800

    测试代码

--- lib/custom_radio.dart
+++ lib/custom_radio.dart
@@ -53,48 +53,48 @@ class RadioItem extends StatelessWidget {
   RadioItem(this._item);
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      margin: new EdgeInsets.all(15.0),
-      child: new Row(
+    return Container(
+      margin: const EdgeInsets.all(15.0),
+      child: Row(
         mainAxisSize: MainAxisSize.max,
         children: <Widget>[
-          new Container(
+          Container(
             height: 50.0,
             width: 50.0,
-            child: new Center(
-              child: new Text(_item.buttonText,
-                  style: new TextStyle(
-                      color:
-                          _item.isSelected ? Colors.white : Colors.black,
-                      //fontWeight: FontWeight.bold,
-                      fontSize: 18.0)),
-            ),
-            decoration: new BoxDecoration(
+            decoration: BoxDecoration(
               color: _item.isSelected
                   ? Colors.blueAccent
                   : Colors.transparent,
-              border: new Border.all(
+              border: Border.all(
                   width: 1.0,
                   color: _item.isSelected
                       ? Colors.blueAccent
                       : Colors.grey),
-              borderRadius: const BorderRadius.all(const Radius.circular(2.0)),
+              borderRadius: const BorderRadius.all(Radius.circular(2.0),),
+            ),
+            child: Center(
+              child: Text(_item.buttonText,
+                  style: TextStyle(
+                      color:
+                          _item.isSelected ? Colors.white : Colors.black,
+                      //fontWeight: FontWeight.bold,
+                      fontSize: 18.0),),
             ),
           ),
-          new Container(
-            margin: new EdgeInsets.only(left: 10.0),
-            child: new Text(_item.text),
+          Container(
+            margin: const EdgeInsets.only(left: 10.0),
+            child:  Text(_item.text),
           )
         ],
       ),
     );
   }
 }
- 
+
 class RadioModel {
   bool isSelected;
   final String buttonText;
   final String text;
- 
+  
   RadioModel(this.isSelected, this.buttonText, this.text);
 }
