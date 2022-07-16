e19e8c37f122cca00439705ddab0ed01fdbb0dc2
yanyangyang <yanyangyang@uenpay.com>
2022-07-16 18:22:25 +0800

    测试代码

--- lib/custom_radio.dart
+++ lib/custom_radio.dart
@@ -1,11 +1,100 @@
 library custom_radio;
 import 'package:flutter/material.dart';
 
-class CustomRadio extends StatelessWidget  {
+class CustomRadio extends StatefulWidget {
   const CustomRadio({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const Text('测试组件');
+  createState() {
+    return CustomRadioState();
   }
+}
+class CustomRadioState extends State<CustomRadio> {
+  List<RadioModel> sampleData = <RadioModel>[];
+ 
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    sampleData.add(RadioModel(false, 'A', 'April 18'));
+    sampleData.add(RadioModel(false, 'B', 'April 17'));
+    sampleData.add(RadioModel(false, 'C', 'April 16'));
+    sampleData.add(RadioModel(false, 'D', 'April 15'));
+  }
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("ListItem"),
+      ),
+      body: ListView.builder(
+        itemCount: sampleData.length,
+        itemBuilder: (BuildContext context, int index) {
+          return InkWell(
+            //highlightColor: Colors.red,
+            splashColor: Colors.blueAccent,
+            onTap: () {
+              setState(() {
+                sampleData.forEach((element) => element.isSelected = false);
+                sampleData[index].isSelected = true;
+              });
+            },
+            child: new RadioItem(sampleData[index]),
+          );
+        },
+      ),
+    );
+  }
+}
+ 
+class RadioItem extends StatelessWidget {
+  final RadioModel _item;
+  RadioItem(this._item);
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: new EdgeInsets.all(15.0),
+      child: new Row(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          new Container(
+            height: 50.0,
+            width: 50.0,
+            child: new Center(
+              child: new Text(_item.buttonText,
+                  style: new TextStyle(
+                      color:
+                          _item.isSelected ? Colors.white : Colors.black,
+                      //fontWeight: FontWeight.bold,
+                      fontSize: 18.0)),
+            ),
+            decoration: new BoxDecoration(
+              color: _item.isSelected
+                  ? Colors.blueAccent
+                  : Colors.transparent,
+              border: new Border.all(
+                  width: 1.0,
+                  color: _item.isSelected
+                      ? Colors.blueAccent
+                      : Colors.grey),
+              borderRadius: const BorderRadius.all(const Radius.circular(2.0)),
+            ),
+          ),
+          new Container(
+            margin: new EdgeInsets.only(left: 10.0),
+            child: new Text(_item.text),
+          )
+        ],
+      ),
+    );
+  }
+}
+ 
+class RadioModel {
+  bool isSelected;
+  final String buttonText;
+  final String text;
+ 
+  RadioModel(this.isSelected, this.buttonText, this.text);
 }
