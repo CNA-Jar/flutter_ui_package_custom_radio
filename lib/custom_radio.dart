d6f5f6000b5bc4abb225958c7c982e69688469e5
yyy <2573120694@qq.com>
2022-07-17 11:41:10 +0800

    测试组件

--- lib/custom_radio.dart
+++ lib/custom_radio.dart
@@ -1,97 +1,38 @@
 library custom_radio;
+
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CustomRadio extends StatefulWidget {
   const CustomRadio({Key? key}) : super(key: key);
 
   @override
   createState() {
-    return CustomRadioState();
-  }
-}
-class CustomRadioState extends State<CustomRadio> {
-  List<RadioModel> sampleData = <RadioModel>[];
- 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    sampleData.add(RadioModel(false, 'A', 'April 18'));
-  }
- 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("ListItem"),
-      ),
-      body: ListView.builder(
-        itemCount: sampleData.length,
-        itemBuilder: (BuildContext context, int index) {
-          return InkWell(
-            //highlightColor: Colors.red,
-            splashColor: Colors.blueAccent,
-            onTap: () {
-              setState(() {
-                sampleData.forEach((element) => element.isSelected = false);
-                sampleData[index].isSelected = true;
-              });
-            },
-            child: new RadioItem(sampleData[index]),
-          );
-        },
-      ),
-    );
-  }
-}
- 
-class RadioItem extends StatelessWidget {
-  final RadioModel _item;
-  RadioItem(this._item);
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(15.0),
-      child: Row(
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          Container(
-            height: 50.0,
-            width: 50.0,
-            decoration: BoxDecoration(
-              color: _item.isSelected
-                  ? Colors.blueAccent
-                  : Colors.transparent,
-              border: Border.all(
-                  width: 1.0,
-                  color: _item.isSelected
-                      ? Colors.blueAccent
-                      : Colors.grey),
-              borderRadius: const BorderRadius.all(Radius.circular(2.0),),
-            ),
-            child: Center(
-              child: Text(_item.buttonText,
-                  style: TextStyle(
-                      color:
-                          _item.isSelected ? Colors.white : Colors.black,
-                      //fontWeight: FontWeight.bold,
-                      fontSize: 18.0),),
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 10.0),
-            child:  Text(_item.text),
-          )
-        ],
-      ),
-    );
+    return _CustomRadioState();
   }
 }
 
-class RadioModel {
-  bool isSelected;
-  final String buttonText;
-  final String text;
+class _CustomRadioState extends State<CustomRadio> {
+  bool _switchActive = false; //单选按钮状态
 
-  RadioModel(this.isSelected, this.buttonText, this.text);
+  void _switchActiveChanged() {
+    setState(() {
+      _switchActive = !_switchActive;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _switchActiveChanged,
+      child: Container(
+        width: 12.sp,
+        height: 12.sp,
+        margin: EdgeInsets.fromLTRB(0.0, 0.0, 6.sp, 0.0),
+        child: _switchActive
+            ? Image.asset('assets/images/selected.png')
+            : Image.asset('assets/images/select.png'),
+      ),
+    );
+  }
 }
